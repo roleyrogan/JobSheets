6f25ef05b90585bca9ddd73bb01cb94f164b8ef8
roleyrogan <ronans.gamboa@gmail.com>
2026-07-03 01:06:59 -0400

    Add CSS

--- script.js
+++ script.js
@@ -12,19 +12,24 @@ let jobs = JSON.parse(localStorage.getItem('jobs')) || [];
 //function to render the jobs from storage
 function renderJobs() {
     jobList.innerHTML = '';
+    //go through every job item in the jobs array and run this function
     jobs.forEach(function(job, index) {
+        //list item
         const li = document.createElement('li');
+        //adds text to the list, and adds a delete button with the delete function
         li.textContent = job.company + ' - ' + job.role + ' - ' + job.status + ' - ' + job.date;
-
         const deleteBtn = document.createElement('button');
         deleteBtn.textContent = 'Delete';
+        //whenever you click on the delete button, it goes to the current index in the 
+        //jobs array and deletes '1' item
         deleteBtn.addEventListener('click', function(){
             jobs.splice(index, 1);
             localStorage.setItem('jobs', JSON.stringify(jobs));
             renderJobs();
         });
-
+        //adds the delete button
         li.appendChild(deleteBtn);
+        //adds the list item to the joblist array 
         jobList.appendChild(li);
     });
 }
